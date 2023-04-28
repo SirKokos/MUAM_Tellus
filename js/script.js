71cc4a6a991c3d14c0b1aab09c089d12c7008c94
SirKokos <102189180+SirKokos@users.noreply.github.com>
2023-04-28 19:34:54 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -35,12 +35,46 @@ if (animItems.length > 0) {
 }
 document.addEventListener("DOMContentLoaded", hiddenCloseclick());
 document.getElementById('click-to-hide').addEventListener("click", hiddenCloseclick);
+document.getElementById('click-to-hide_1').addEventListener("click", hiddenCloseclick_1);
+document.getElementById('click-to-hide_2').addEventListener("click", hiddenCloseclick_2);
+document.getElementById('click-to-hide_3').addEventListener("click", hiddenCloseclick_3);
+document.getElementById('click-to-hide_4').addEventListener("click", hiddenCloseclick_4);
 function hiddenCloseclick() {
     let x = document.getElementById('hidden-element');
     if (x.style.display == "none"){
-        x.style.display = "flex";
-        x.style.justifyContent = "centre"
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"}
+
+};
+function hiddenCloseclick_1(){
+    let x = document.getElementById('hidden-element_1');
+    if (x.style.display == "none"){
+        x.style.display = "block";
     } else {
         x.style.display = "none"}
 };
 
+function hiddenCloseclick_2(){
+    let x = document.getElementById('hidden-element_2');
+    if (x.style.display == "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"}
+};
+function hiddenCloseclick_3(){
+    let x = document.getElementById('hidden-element_3');
+    if (x.style.display == "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"}
+
+};
+function hiddenCloseclick_4(){
+    let x = document.getElementById('hidden-element_4');
+    if (x.style.display == "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"}
+
+};
